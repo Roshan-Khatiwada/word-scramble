23db20dfc275a22dea8f9961c17bc7dc0e3e42b9
Roshan Khatiwada <riteshrooson@gmail.com>
2024-10-24 12:11:27 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WordScramble from './components/WordScramble';
+
+function App() {
+  return (
+    <div className="App">
+       <WordScramble />
+    </div>
+  );
+}
+
+export default App;
